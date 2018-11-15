d92e5467b0c89ffba243f1143c7dcefd7fb3cf09
Bandera <42476974+Bandera1@users.noreply.github.com>
2018-11-15 16:52:11 +0200

    Добавьте файлы проекта.

---
+++ Динамічні масиви/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+#include <Windows.h>
+#include <iomanip>
+#include <math.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
---
+++ Динамічні масиви/Main.cpp
@@ -0,0 +1,21 @@
+#include "Header.h"
+
+//----------------------
+//-----Dynamicall array homeworks
+//----------------------	
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+//-------------First---------------
+	int *length = new int, *width = new int;
+	cout << "Enter length of array: ";
+	cin >> *length;
+	cout << "Enter width of array: ";
+	cin >> *width;
+
+	int **arr = new int*[width];
+	system("pause");
+}
